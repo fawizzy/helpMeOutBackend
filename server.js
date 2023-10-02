9b9dda88c2917a49a995b8642bf845835b103612
fawaz <oduolafawaz@gmail.com>
2023-10-02 12:09:57 +0200

    documentation

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const videoRoute = require("./routes/videoRoute")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const path = require("path")
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./api/swagger/swagger.yaml');
 
 // Create an instance of Express
 const app = express();
 // Define a port for your server to listen on
 const port = process.env.PORT || 3000;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: 100000, parameterLimit: 5 }));
